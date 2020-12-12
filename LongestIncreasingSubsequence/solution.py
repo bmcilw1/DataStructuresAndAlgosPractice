6e8c9b8cd5f1cb32f20eac94c19dbd4c75f5ec04
Brian McIlwain <technicallyemployed@gmail.com>
2020-12-12 17:27:33 -0600

    Force python3 shebang

--- LongestIncreasingSubsequence/solution.py
+++ LongestIncreasingSubsequence/solution.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 class Solution:
     def lengthOfLIS(self, nums: list[int]) -> int:
         return 1
--- LongestIncreasingSubsequence/test_solution.py
+++ LongestIncreasingSubsequence/test_solution.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python3
+
 from solution import Solution
 
+@pytest.fixture()
+def solution():
+    yield Solution()
+
 class TestSolution:
-    def test_lengthOfLIS_whenPassedOneNumber_returnsOne(self):
-        solution = Solution()
+    def test_lengthOfLIS_whenPassedOneNumber_returnsOne(self, solution):
         assert solution.lengthOfLIS([1]) == 1
