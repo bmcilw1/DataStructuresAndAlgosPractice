f499e87d855b28934566d56b026a3334ce4dc209
Brian McIlwain <technicallyemployed@gmail.com>
2020-12-12 18:49:38 -0600

    First attempt

--- LongestIncreasingSubsequence/solution.py
+++ LongestIncreasingSubsequence/solution.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
 
 class Solution:
-    def lengthOfLIS(self, nums: list[int]) -> int:
-        return 1
+    def lengthOfLongestIncreasingSubsequence(self, nums: list[int]) -> int:
+        N = len(nums)
+        lis = [1] * (N + 1)
+
+        for idx, n in enumerate(nums, start=1):
+            subs = []
+
+            for idx_sub in range(0, idx):
+                if nums[idx_sub] < n:
+                    subs[idx_sub] = lis[idx_sub]
+
+            lis[idx] = 1 + max(subs, default=0)
+
+        return max(lis, default=0)
--- LongestIncreasingSubsequence/test_solution.py
+++ LongestIncreasingSubsequence/test_solution.py
@@ -11,13 +11,16 @@ def solution():
 
 class TestSolution:
     def test_lengthOfLIS_whenPassedOneNumber_returnsOne(self, solution):
-        assert solution.lengthOfLIS([8]) == 1
+        assert solution.lengthOfLongestIncreasingSubsequence([8]) == 1
 
     def test_lengthOfLIS_exampleOne(self, solution):
-        assert solution.lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]) == 4
+        assert solution.lengthOfLongestIncreasingSubsequence(
+            [10, 9, 2, 5, 3, 7, 101, 18]) == 4
 
     def test_lengthOfLIS_exampleTwo(self, solution):
-        assert solution.lengthOfLIS([0, 1, 0, 3, 2, 3]) == 4
+        assert solution.lengthOfLongestIncreasingSubsequence(
+            [0, 1, 0, 3, 2, 3]) == 4
 
     def test_lengthOfLIS_exampleThree(self, solution):
-        assert solution.lengthOfLIS([7, 7, 7, 7, 7, 7, 7]) == 1
+        assert solution.lengthOfLongestIncreasingSubsequence(
+            [7, 7, 7, 7, 7, 7, 7]) == 1
