72161878c41f6574db6b3f857a83ca695cd13ca6
Brian McIlwain <technicallyemployed@gmail.com>
2020-12-12 18:01:11 -0600

    Added given tests

--- LongestIncreasingSubsequence/test_solution.py
+++ LongestIncreasingSubsequence/test_solution.py
@@ -12,3 +12,12 @@ def solution():
 class TestSolution:
     def test_lengthOfLIS_whenPassedOneNumber_returnsOne(self, solution):
         assert solution.lengthOfLIS([8]) == 1
+
+    def test_lengthOfLIS_exampleOne(self, solution):
+        assert solution.lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]) == 4
+
+    def test_lengthOfLIS_exampleTwo(self, solution):
+        assert solution.lengthOfLIS([0, 1, 0, 3, 2, 3]) == 4
+
+    def test_lengthOfLIS_exampleThree(self, solution):
+        assert solution.lengthOfLIS([7, 7, 7, 7, 7, 7, 7]) == 1
