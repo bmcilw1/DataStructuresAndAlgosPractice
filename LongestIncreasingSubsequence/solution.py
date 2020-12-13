9bb3693f2f829ae6a7db6cad66b742550ce00ae5
Brian McIlwain <technicallyemployed@gmail.com>
2020-12-12 19:19:03 -0600

    Rename for clarity

--- LongestIncreasingSubsequence/solution.py
+++ LongestIncreasingSubsequence/solution.py
@@ -5,7 +5,7 @@ class Solution:
         lis = [1] * len(nums)
 
         for i in range(1, len(nums)):
-            subs = [lis[j] for j in range(0, i) if nums[j] < nums[i]]
-            lis[i] = 1 + max(subs, default=0)
+            lis_before_i = [lis[j] for j in range(0, i) if nums[j] < nums[i]]
+            lis[i] = 1 + max(lis_before_i, default=0)
 
         return max(lis, default=0)
