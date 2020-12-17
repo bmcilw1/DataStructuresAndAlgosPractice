5494f187a84c43d45da39ac633eb9e0165ec3d58
Brian McIlwain <technicallyemployed@gmail.com>
2020-12-17 13:41:48 -0600

    Added memoized scaffold

--- CoinChange/solution.py
+++ CoinChange/solution.py
@@ -2,14 +2,21 @@
 
 class Solution:
     coins = set()
+    past_change = dict()
 
     def coinChange(self, coins: list[int], amount: int) -> int:
         self.coins = set(coins)
         return self.change(amount)
 
     def change(self, amount: int) -> int:
+        if amount in self.past_change:
+            return self.past_change[amount]
+
         if amount == 0:
-            return 0
+            self.past_change[amount] = 0
         elif amount in self.coins:
-            return 1
-        return -1
+            self.past_change[amount] = 1
+        else:
+            self.past_change[amount] = -1
+
+        return self.past_change[amount]
