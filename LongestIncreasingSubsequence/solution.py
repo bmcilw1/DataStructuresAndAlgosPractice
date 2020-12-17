7cc1f21a8aa8edc7f39f70e4e0af52bd01f4d10d
Brian McIlwain <technicallyemployed@gmail.com>
2020-12-17 11:19:24 -0600

    Changed counters to match walkthrough

--- LongestIncreasingSubsequence/solution.py
+++ LongestIncreasingSubsequence/solution.py
@@ -7,9 +7,9 @@ class Solution:
     def lengthOfLongestIncreasingSubsequence_n_squared(self, nums: list[int]) -> int:
         lis = [1] * len(nums)
 
-        for i in range(1, len(nums)):
+        for k in range(1, len(nums)):
             prior_LIS_with_num_less_than_i = [
-                lis[j] for j in range(i) if nums[j] < nums[i]]
-            lis[i] = 1 + max(prior_LIS_with_num_less_than_i, default=0)
+                lis[j] for j in range(k) if nums[j] < nums[k]]
+            lis[k] = 1 + max(prior_LIS_with_num_less_than_i, default=0)
 
         return lis[-1]
